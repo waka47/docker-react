7c7d10988e6d1fcbd1f66f381b5dba65dd6e2690
Wakaflocka <60948091+waka47@users.noreply.github.com>
2020-02-11 16:09:15 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Waqas is great!
         </p>
         <a
           className="App-link"
